1c6b36e4128c88324ae89b355ec3723b95207001
Ilian Nilsson <34071280+IlleNilsson@users.noreply.github.com>
2026-06-06 10:51:16 +0200

    Register webhook and polling receive modules

--- src/receive_modules.rs
+++ src/receive_modules.rs
@@ -1,13 +1,21 @@
 pub mod file;
+pub mod ftp;
+pub mod ftps;
 pub mod http;
 pub mod msmq;
 pub mod rabbitmq;
+pub mod sftp;
+pub mod webhook;
 
 use crate::receive_claims::ReceiveClaim;
 use file::FileReceiveModule;
+use ftp::FtpReceiveModule;
+use ftps::FtpsReceiveModule;
 use http::HttpReceiveModule;
 use msmq::MsmqReceiveModule;
 use rabbitmq::RabbitMqReceiveModule;
+use sftp::SftpReceiveModule;
+use webhook::WebHookReceiveModule;
 
 pub trait ReceiveEndpointModule {
     fn name(&self) -> &'static str;
@@ -26,9 +34,13 @@ pub struct ReceivedStream {
 pub fn load_receive_module(name: &str) -> Box<dyn ReceiveEndpointModule> {
     match name {
         "file" => Box::new(FileReceiveModule),
+        "ftp" => Box::new(FtpReceiveModule),
+        "ftps" => Box::new(FtpsReceiveModule),
         "http" => Box::new(HttpReceiveModule),
         "msmq" => Box::new(MsmqReceiveModule),
         "rabbitmq" => Box::new(RabbitMqReceiveModule),
-        other => panic!("unknown receive endpoint module: {other}. Use 'http', 'file', 'msmq', or 'rabbitmq'."),
+        "sftp" => Box::new(SftpReceiveModule),
+        "webhook" => Box::new(WebHookReceiveModule),
+        other => panic!("unknown receive endpoint module: {other}"),
     }
 }
