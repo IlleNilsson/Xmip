b2b44c315ace68fbb2bf8cbb08413d62e2f966b1
Ilian Nilsson <ilian.nilsson@consid.se>
2026-06-12 06:54:29 +0200

    Remove tracked Cargo build artifacts

--- target/debug/build/anyhow-ef3f532a3e874254/build_script_build-ef3f532a3e874254.d
+++
@@ -1,5 +0,0 @@
-C:\Users\IlianNilsson\OneDrive\Repos\Xmip\target\debug\build\anyhow-ef3f532a3e874254\build_script_build-ef3f532a3e874254.d: C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs
-
-C:\Users\IlianNilsson\OneDrive\Repos\Xmip\target\debug\build\anyhow-ef3f532a3e874254\build_script_build-ef3f532a3e874254.exe: C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs
-
-C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\anyhow-1.0.102\build.rs:
--- target/debug/deps/either-d954eb22b620f84a.d
+++
@@ -1,9 +0,0 @@
-C:\Users\IlianNilsson\OneDrive\Repos\Xmip\target\debug\deps\either-d954eb22b620f84a.d: C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\lib.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\iterator.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\into_either.rs
-
-C:\Users\IlianNilsson\OneDrive\Repos\Xmip\target\debug\deps\libeither-d954eb22b620f84a.rlib: C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\lib.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\iterator.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\into_either.rs
-
-C:\Users\IlianNilsson\OneDrive\Repos\Xmip\target\debug\deps\libeither-d954eb22b620f84a.rmeta: C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\lib.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\iterator.rs C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\into_either.rs
-
-C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\lib.rs:
-C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\iterator.rs:
-C:\Users\IlianNilsson\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\either-1.16.0\src\into_either.rs:
