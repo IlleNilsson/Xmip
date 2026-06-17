8ff55fad223b12180a4cd9e68510bc8ef330d4eb
Ilian Nilsson <34071280+IlleNilsson@users.noreply.github.com>
2026-06-17 23:56:18 +0200

    Expose module loading modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 pub mod handler;
+pub mod handler_registry;
 pub mod management_store;
+pub mod module_loader;
+pub mod module_manifest;
 pub mod runtime_message;
 pub mod runtime_store;
 pub mod threaded_runtime;
