b2678cdb5b76646978693373c0cdb6976090ac96
Ilian Nilsson <34071280+IlleNilsson@users.noreply.github.com>
2026-07-06 15:17:42 +0200

    Export core contracts from xmip-core

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+pub mod contracts;
 pub mod journey_model;
 pub mod transport_technology;
 pub mod vertical_slice;
+
+pub use contracts::{
+    ContentCreatedMessage, ContentHandler, ContentHandlerInvocation, ContentHandlerResult,
+    ContentOperation, ContentPropertySelector, ContentSelector, ContentSelectorSegment,
+    DemotedProperty, DemotionTarget, ExecutionHostKind, ExtensionEntrypoint, ExtensionManifest,
+    HandlerInvocation, HandlerResult, HandlerStatus, Module, ModuleCapability, ModuleEntrypoint,
+    ModuleIdentity, ModuleKind, ModuleManifest, PromotedProperty, SelectorEvaluation,
+    TransportHandler, XmipModule,
+};
 
 pub use journey_model::{
     append_message_to_journey, create_derived_message, create_initial_message,
@@ -10,5 +20,6 @@ pub use journey_model::{
 };
 
 pub use transport_technology::{
-    core_transport_tree, depends_on, TransportTechnology, TransportTechnologyLayer,
+    core_transport_tree, depends_on, file_transport_tree, family_of, ip_transport_tree,
+    TransportEventKind, TransportTechnology, TransportTechnologyFamily, TransportTechnologyLayer,
 };
