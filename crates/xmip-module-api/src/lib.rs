5d9aaffe4e3a4c08993213dcc692d7efb832461c
Ilian Nilsson <34071280+IlleNilsson@users.noreply.github.com>
2026-07-06 15:21:10 +0200

    Use renamed xmip-abi crate in module API

--- crates/xmip-module-api/src/lib.rs
+++ crates/xmip-module-api/src/lib.rs
@@ -1,6 +1,6 @@
 pub use xmip_core::contracts::*;
 
-pub use xmip_module_abi::{
+pub use xmip_abi::{
     validate_module_abi, ModuleAbiDescriptor, ModuleAbiKind, XMIP_MODULE_ABI_VERSION,
     XMIP_MODULE_ENTRYPOINT,
 };
